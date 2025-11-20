75141f5453f38cfed25958b71e085aa1ce4de339
Jackson <jacksonhenmry3@gmail.com>
2025-11-19 19:21:41 -0500

    this is silly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jacksonhenry.net',
+  site: 'https://jacksonhenry3.github.io',
   base: '/StrongTownsWorcester',
+  build: {
+    format: 'file',
+    outDir: 'docs'
+  }
 });
